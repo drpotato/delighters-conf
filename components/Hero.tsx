01a480c3f86a5771e3346ab15b24064ced0b7be0
Chris Morgan <morgan@dovetailapp.com>
2022-07-25 22:24:29 +1000

    Fix hero image padding

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,18 +4,25 @@ import Image from "next/image";
 import styles from "./Hero.module.css";
 
 const Hero = ({}) => {
-  return <>
-    <section className={styles.hero}>
-      <span className={styles.left}>Delighters Conference 2022</span>
-      <span className={styles.right}>Experience is Everything</span>
-    </section>
-    <span>
-      Secure your spot and connect with some of Sydney’s best product engineers
-    </span>
-    <div style={{width: "100%"}}>
-      <img style={{width: "100%"}} alt="Hero" src="/hero.png"/>
-    </div>
-  </>;
+  return (
+    <>
+      <section className={styles.hero}>
+        <span className={styles.left}>Delighters Conference 2022</span>
+        <span className={styles.right}>Experience is Everything</span>
+      </section>
+      <span>
+        Secure your spot and connect with some of Sydney’s best product
+        engineers
+      </span>
+      <div style={{ width: "100%" }}>
+        <img
+          style={{ display: "block", width: "100%" }}
+          alt="Hero"
+          src="/hero.png"
+        />
+      </div>
+    </>
+  );
 };
 
-export default Hero
+export default Hero;
