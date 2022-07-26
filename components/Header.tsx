841856b1e5290c686db71a2022a8fa3a9f8d0dcb
Chris Morgan <morgan@dovetailapp.com>
2022-07-27 09:39:35 +1000

    Update header to new design

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,82 @@ import styles from "./Header.module.css";
 import Logo from "../public/logo.svg";
 
 const Header = () => {
-  return <header className={styles.header}>
-    <Logo />
-    <div className={styles.links}>
-      <Link href="/about">What is Delighters?</Link>
-      <Link href="/speakers">Speakers</Link>
-      <Link href="/sign-up"><a className={styles.button} >Sign Up</a></Link>
-    </div>
-  </header>
-}
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <div style={{ marginTop: "8px" }}>
+          <Logo />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "15px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              border: "#000F9F 1px solid",
+              borderTop: "none",
+            }}
+          >
+            <Link href="/about">
+              <a
+                style={{
+                  display: "flex",
+                  justifyItems: "space-between",
+                  gap: "26px",
+                  padding: "10px 8px 7px 12px",
+                  borderRight: "#000F9F 1px solid",
+                }}
+              >
+                <span>What is Delighters?</span>
+                <span style={{ width: "16px", textAlign: "center" }}>↓</span>
+              </a>
+            </Link>
+            <Link href="/speakers">
+              <a
+                style={{
+                  display: "flex",
+                  justifyItems: "space-between",
+                  gap: "26px",
+                  padding: "10px 8px 7px 12px",
+                }}
+              >
+                <span>Speakers</span>
+                <span style={{ width: "16px", textAlign: "center" }}>↓</span>
+              </a>
+            </Link>
+            <Link href="/sign-up">
+              <a
+                style={{
+                  display: "flex",
+                  justifyItems: "space-between",
+                  gap: "26px",
+                  padding: "10px 8px 7px 12px",
+                  background: "#000F9F",
+                  color: "#ffffff",
+                }}
+              >
+                <span>Sign Up</span>
+                <span style={{ width: "16px", textAlign: "center" }}>↗</span>
+              </a>
+            </Link>
+          </div>
+          <span
+            style={{
+              textAlign: "right",
+              fontSize: "10px",
+              textTransform: "capitalize",
+            }}
+          >
+            Brought to you by Dovetail
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
