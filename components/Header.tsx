36c18dd36ed2cb8914dc406de014c2900123b33e
Chris Morgan <morgan@dovetailapp.com>
2022-08-02 13:30:44 +1000

    Use inline function definition for Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,65 +4,62 @@ import Link from "next/link";
 import styles from "./Header.module.css";
 import Logo from "../public/logo.svg";
 
-const Header = React.forwardRef<HTMLElement>((_, ref) => {
-  console.log(styles.linkLight);
-  return (
-    <header ref={ref} className={styles.header}>
-      <div className={styles.inner}>
-        <div
-          style={{
-            paddingTop: "8px",
-            flexGrow: 1,
-            borderTop: "#000F9F 1px solid",
-          }}
-        >
-          <Logo />
-        </div>
+const Header = React.forwardRef<HTMLElement>((_, ref) => (
+  <header ref={ref} className={styles.header}>
+    <div className={styles.inner}>
+      <div
+        style={{
+          paddingTop: "8px",
+          flexGrow: 1,
+          borderTop: "#000F9F 1px solid",
+        }}
+      >
+        <Logo />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+        }}
+      >
         <div
           style={{
             display: "flex",
-            flexDirection: "column",
-            gap: "15px",
           }}
         >
-          <div
-            style={{
-              display: "flex",
-            }}
-          >
-            <Link href="/about" scroll={false}>
-              <a className={styles.linkLight}>
-                <span>What is Delighters?</span>
-                <span style={{ width: "16px", textAlign: "center" }}>↓</span>
-              </a>
-            </Link>
-            <Link href="/speakers" scroll={false}>
-              <a className={styles.linkLight}>
-                <span>Speakers</span>
-                <span style={{ width: "16px", textAlign: "center" }}>↓</span>
-              </a>
-            </Link>
-            <Link href="/sign-up" scroll={false}>
-              <a className={styles.linkDark}>
-                <span>Sign Up</span>
-                <span style={{ width: "16px", textAlign: "center" }}>↗</span>
-              </a>
-            </Link>
-          </div>
-          <span
-            style={{
-              textAlign: "right",
-              fontSize: "10px",
-              textTransform: "capitalize",
-            }}
-          >
-            Brought to you by Dovetail
-          </span>
+          <Link href="/about" scroll={false}>
+            <a className={styles.linkLight}>
+              <span>What is Delighters?</span>
+              <span style={{ width: "16px", textAlign: "center" }}>↓</span>
+            </a>
+          </Link>
+          <Link href="/speakers" scroll={false}>
+            <a className={styles.linkLight}>
+              <span>Speakers</span>
+              <span style={{ width: "16px", textAlign: "center" }}>↓</span>
+            </a>
+          </Link>
+          <Link href="/sign-up" scroll={false}>
+            <a className={styles.linkDark}>
+              <span>Sign Up</span>
+              <span style={{ width: "16px", textAlign: "center" }}>↗</span>
+            </a>
+          </Link>
         </div>
+        <span
+          style={{
+            textAlign: "right",
+            fontSize: "10px",
+            textTransform: "capitalize",
+          }}
+        >
+          Brought to you by Dovetail
+        </span>
       </div>
-    </header>
-  );
-});
+    </div>
+  </header>
+));
 
 Header.displayName = "Header";
 
